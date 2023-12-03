1ec6f2faabedd5eeae877ad338fcd4a81e9f46c9
Andresrodriguez26 <anferome10@gmail.com>
2023-12-02 22:30:15 -0500

    needs review

--- Day-3/dist/index.js
+++ Day-3/dist/index.js
@@ -1,81 +1,146 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 class Item {
-    constructor(id, name, price, description) {
-        this.id = id;
+    constructor(name, price, description) {
         this.name = name;
         this.price = price;
         this.description = description;
+        this.id = (0, uuid_1.v4)();
     }
 }
 class User {
-    constructor(id, name, age, cart) {
-        this.id = id;
+    get id_1() {
+        return this._id;
+    }
+    constructor(name, age) {
         this.name = name;
         this.age = age;
-        this.cart = cart;
+        this.cart = [];
+        this._id = (0, uuid_1.v4)();
+    }
+    addToCart(item) {
+        this.cart.push(item);
+    }
+    removeFromCart(item, user) {
+        user.cart = user.cart.filter((cartItem) => cartItem.id !== item.id);
+    }
+    printCart(user) {
+        console.log(`Your Cart: `);
+        user.cart.forEach((item) => {
+            console.log(`${item.name} with of Price: $${item.price}`);
+        });
     }
 }
 const uuid_1 = require("uuid");
-function createUser(name, age) {
-    let user = {
-        id: (0, uuid_1.v4)(),
-        name,
-        age,
-        cart: []
-    };
-    return user;
+const rl = __importStar(require("readline-sync"));
+function createUser() {
+    let name = "";
+    let age = 0;
+    name = rl.question("Please enter the name of the user: ");
+    while (true) {
+        age = parseInt(rl.question("Enter your age: "));
+        // // Validate age input
+        if (isNaN(age)) {
+            console.log("Invalid age. Please enter a valid number.");
+        }
+        else {
+            break;
+        }
+    }
+    return new User(name, age);
 }
-function createItem(name, price, description) {
-    let item = {
-        id: (0, uuid_1.v4)(),
-        name,
-        price,
-        description
-    };
-    return item;
+console.log(createUser());
+function createItem() {
+    let name = "";
+    let price = 0;
+    let description = "";
+    name = rl.question("Please enter the name of the item: ");
+    description = rl.question("Please enter a description for your item");
+    while (true) {
+        price = parseInt(rl.question("Enter the price: "));
+        // // Validate age input
+        if (isNaN(price)) {
+            console.log("Invalid price. Please enter a valid price.");
+        }
+        else {
+            break;
+        }
+    }
+    return new Item(name, price, description);
 }
-function addToCart(item, user) {
-    user.cart.push(item);
-}
-function removeFromCart(item, user) {
-    user.cart = user.cart.filter((cartItem) => cartItem.id !== item.id);
-}
-function printCart(user) {
-    console.log(`Your Cart`);
-    user.cart.forEach((item) => {
-        console.log(`${item.name} & Price: ${item.price}`);
-    });
-}
+console.log(createItem());
+//  ------------------------- New Driver ----------------------------
+// driverCode(): void  {
+//     const _user = createUser()
+//     while (true) {
+//         const _response: string = prompt("What would you like to do? add, delete, view ")
+//         if (_response.toLowerCase() == "add") {
+// let newItem = createItem()
+//             _user.addToCart(newItem)
+//         }
+//         else if (_response.toLowerCase() === "delete") {
+//             _user.removeFromCart();
+//         } else if (_response.toLowerCase() == "view") {
+//             _usert.printCart();
+//         } else if (_response.toLowerCase() == "quit") {
+//             break;
+//         } else {
+//             console.log("Please enter a valid response");
+//         }
+//     }
+// }
 // ----------------- Driver Code --------------------- // 
-let user = createUser("Andres Rodriguez", 25);
-let firstItem = createItem("Apple", 2.99, "Red and Juicy");
-let secondItem = createItem("Orange Fanta", 4.99, "Orange and Tasty");
-let thirdItem = createItem("Pencil", 0.99, "Sharp");
-addToCart(firstItem, user);
-printCart(user);
-let total = 0;
-for (let item of user.cart) {
-    total += item.price;
-}
-let _total = total.toFixed(2);
-console.log(`First Total: $${_total}`);
-addToCart(firstItem, user);
-addToCart(secondItem, user);
-addToCart(thirdItem, user);
-let secondTotal = 0;
-for (let item of user.cart) {
-    secondTotal += item.price;
-}
-let newTotal = secondTotal.toFixed(2);
-console.log(`New Total: $${newTotal}`);
-removeFromCart(firstItem, user);
-removeFromCart(secondItem, user);
-removeFromCart(thirdItem, user);
-printCart(user);
-let finalTotal = 0;
-for (let item of user.cart) {
-    finalTotal += item.price;
-}
-let formattedTotal = finalTotal.toFixed(2);
-console.log(`Final Total: $${formattedTotal}`);
+// let user = createUser("Andres Rodriguez", 25)
+// let firstItem = createItem("Apple", 2.99, "Red and Juicy")
+// let secondItem = createItem("Orange Fanta", 4.99, "Orange and Tasty")
+// let thirdItem = createItem("Pencil", 0.99, "Sharp")
+// addToCart(firstItem, user)
+// printCart(user)
+// let total = 0 
+// for (let item of user.cart) {
+//    total += item.price
+// }
+// let _total = total.toFixed(2)
+// console.log(`First Total: $${_total}`)
+// addToCart(firstItem, user)
+// addToCart(secondItem, user)
+// addToCart(thirdItem, user)
+// let secondTotal = 0
+// for (let item of user.cart) {
+//     secondTotal += item.price
+// }
+// let newTotal = secondTotal.toFixed(2);
+// console.log(`New Total: $${newTotal}`);
+// removeFromCart(firstItem, user)
+// removeFromCart(secondItem, user)
+// removeFromCart(thirdItem, user)
+// printCart(user)
+// let finalTotal = 0
+// for (let item of user.cart) {
+//   finalTotal += item.price;
+// }
+// let formattedTotal = finalTotal.toFixed(2);
+// console.log(`Final Total: $${formattedTotal}`);
--- Day-3/src/index.ts
+++ Day-3/src/index.ts
@@ -1,103 +1,195 @@
 class Item {
-    constructor(public readonly id:string,
-                public name: string,
+
+    public readonly id:string
+
+    constructor(public name: string,
                 public price: number,
-                public description: string){}
+                public description: string){
+                    this.id = uuid()}
 }
 
 
 class User {
-    constructor(public readonly id:string,
-                public name: string,
-                public age: number,
-                public cart: Item[]){}
+
+    private readonly _id: string
+
+    public get id_1(): string {
+        return this._id
+    }
+
+    // cart: Item[] = []
+    cart: { [itemName: string]: Item } = {};
+
+
+    constructor(public name: string,
+                public age: number){
+                    this._id = uuid()}
+                
+    // addToCart(item:Item): void {
+    //     this.cart.push(item)
+    // }
+
+    addToCart(itemName: string, item: Item): void {
+        this.cart[itemName] = item;
+    }
+    
+    // removeFromCart(item:Item, user:User): void {
+    //     user.cart = user.cart.filter((cartItem) => cartItem.id !== item.id)
+    // }
+
+    removeFromCart(): void {
+    
+       let itemDelete: string = rl.question("What item would you like to delete?: ")
+
+        if (itemDelete in this.cart){
+            delete this.cart[itemDelete]
+            console.log(`${itemDelete} was removed from your cart `)
+        } else {
+            console.log(`${itemDelete} was not found in your cart, please check your items `)
+        }
+
+        
+    }
+    
+    printCart(user:User): void {
+        console.log(`Your Cart: `)
+        user.cart.forEach((item) => {
+            console.log(`${item.name} with of Price: $${item.price}`)
+        })
+    }
+
 }
 
 import { v4 as uuid } from "uuid"
+import * as rl from 'readline-sync'
 
-function createUser(name:string, age:number): User {
+
+function createUser(): User {
+   let name:string = ""
+   let age:number = 0
+
+   name = rl.question("Please enter the name of the user: ")
+
+   while(true){ 
     
-    let user: User = {
-        id: uuid(),
-        name,
-        age,
-        cart:[]
-    }
-    return user
-}
-
-function createItem(name:string, price:number, description:string): Item {
+    age = parseInt(rl.question("Enter your age: "))
     
-    let item: Item = {
-        id: uuid(),
-        name,
-        price,
-        description
+    // // Validate age input
+    if (isNaN(age)) {
+        console.log("Invalid age. Please enter a valid number.");
     }
-    return item
+
+    else{
+        break
+    }
+    
+}
+    return  new User(name, age)
 }
 
-function addToCart(item:Item, user:User): void {
-    user.cart.push(item)
+console.log(createUser())
+
+function createItem(): Item {
+    let name: string = ""
+    let price: number = 0
+    let description: string = ""
+
+    name = rl.question("Please enter the name of the item: ")
+    description = rl.question("Please enter a description for your item")
+
+    while(true){ 
+    
+        price = parseInt(rl.question("Enter the price: "))
+
+        // // Validate age input
+        if (isNaN(price)) {
+            console.log("Invalid price. Please enter a valid price.");
+        }
+    
+        else{
+            break
+        }
+        
+    }
+        return new Item (name, price, description)
+    }
+
+console.log(createItem())
+   
+
+//  ------------------------- New Driver ----------------------------
+
+function driverCode() {
+    const _user = createUser()
+
+    while (true) {
+        const _response: string = rl.question("What would you like to do? add, delete, view ")
+
+        if (_response.toLowerCase() == "add") {
+                let newItem = createItem()
+            _user.addToCart(newItem, Item)
+        }
+        else if (_response.toLowerCase() == "delete") {
+            _user.removeFromCart();
+        } else if (_response.toLowerCase() == "view") {
+            _user.printCart();
+        } else if (_response.toLowerCase() == "quit") {
+            break;
+        } else {
+            console.log("Please enter a valid response");
+        }
+    }
+
 }
 
-function removeFromCart(item:Item, user:User): void {
-    user.cart = user.cart.filter((cartItem) => cartItem.id !== item.id)
-}
-
-function printCart(user:User): void {
-    console.log(`Your Cart: `)
-    user.cart.forEach((item) => {
-        console.log(`${item.name} with of Price: $${item.price}`)
-    })
-}
+console.log(driverCode())
 
 
 // ----------------- Driver Code --------------------- // 
 
-let user = createUser("Andres Rodriguez", 25)
+// let user = createUser("Andres Rodriguez", 25)
 
-let firstItem = createItem("Apple", 2.99, "Red and Juicy")
-let secondItem = createItem("Orange Fanta", 4.99, "Orange and Tasty")
-let thirdItem = createItem("Pencil", 0.99, "Sharp")
+// let firstItem = createItem("Apple", 2.99, "Red and Juicy")
+// let secondItem = createItem("Orange Fanta", 4.99, "Orange and Tasty")
+// let thirdItem = createItem("Pencil", 0.99, "Sharp")
 
-addToCart(firstItem, user)
+// addToCart(firstItem, user)
 
-printCart(user)
+// printCart(user)
 
-let total = 0 
+// let total = 0 
 
-for (let item of user.cart) {
-   total += item.price
-}
+// for (let item of user.cart) {
+//    total += item.price
+// }
 
-let _total = total.toFixed(2)
-console.log(`First Total: $${_total}`)
+// let _total = total.toFixed(2)
+// console.log(`First Total: $${_total}`)
 
-addToCart(firstItem, user)
-addToCart(secondItem, user)
-addToCart(thirdItem, user)
+// addToCart(firstItem, user)
+// addToCart(secondItem, user)
+// addToCart(thirdItem, user)
 
-let secondTotal = 0
+// let secondTotal = 0
 
-for (let item of user.cart) {
-    secondTotal += item.price
-}
+// for (let item of user.cart) {
+//     secondTotal += item.price
+// }
 
-let newTotal = secondTotal.toFixed(2);
-console.log(`New Total: $${newTotal}`);
+// let newTotal = secondTotal.toFixed(2);
+// console.log(`New Total: $${newTotal}`);
 
-removeFromCart(firstItem, user)
-removeFromCart(secondItem, user)
-removeFromCart(thirdItem, user)
+// removeFromCart(firstItem, user)
+// removeFromCart(secondItem, user)
+// removeFromCart(thirdItem, user)
 
-printCart(user)
+// printCart(user)
 
-let finalTotal = 0
+// let finalTotal = 0
 
-for (let item of user.cart) {
-  finalTotal += item.price;
-}
+// for (let item of user.cart) {
+//   finalTotal += item.price;
+// }
 
-let formattedTotal = finalTotal.toFixed(2);
-console.log(`Final Total: $${formattedTotal}`);
+// let formattedTotal = finalTotal.toFixed(2);
+// console.log(`Final Total: $${formattedTotal}`);
